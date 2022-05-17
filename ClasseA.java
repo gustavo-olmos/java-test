612b56d2326363e8b860f184a79e4425808250d5
Gustavo L O Ferraz <87102570+gustavo-olmos@users.noreply.github.com>
2022-05-16 21:44:16 -0300

    ok, commited.

---
+++ ClasseA.java
@@ -0,0 +1,12 @@
+package br.com.letscode;
+
+public class ClasseA{
+
+    public static void main(String[] args) {
+        System.out.println("Commit Alterado!!!....");
+        System.out.println("Nova alteração!!!");
+
+        System.out.println("Projeto teste via GitHub");
+    }
+
+}
