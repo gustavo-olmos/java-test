401316f2bdcf0bcea04fc9da58334429fc436026
Gustavo L O Ferraz <87102570+gustavo-olmos@users.noreply.github.com>
2022-06-22 00:03:42 -0300

    package solid.notasedisc

--- LancarNota.java
+++ LancarNota.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class LancarNota {
+    
     public void lancamento(List<String> discip, Hashtable<String, Integer> discNt, Scanner sc){
         for (String s : discip) {
             System.out.println("Lance nota à disciplina " + s);
@@ -16,6 +17,7 @@ public class LancarNota {
             discNt.put(s, nt);
         }
     }
+    
     public static void situacao(int nt){
         if (nt >= 5 && nt <= 10){
             System.out.println("APROVADO");
